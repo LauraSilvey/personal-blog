ed9daf753e00a5b78f05848766c19357bbfdfa58
Laura Silvey <lsilvey@gmail.com>
2018-02-01 16:56:50 -0800

    styling

--- models/post.js
+++ models/post.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
 var PostSchema = new mongoose.Schema({
-  author: String,
   title: String,
   entry: String,
+  created: {type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model("Post", PostSchema);
